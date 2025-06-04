a2848213d7272584cf17815f9ff9a216206b8037
Rajitha <rajitha.jaltary@gmail.com>
2025-06-04 21:00:21 +0530

    changes regarding login API

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const InitializeDB = require("./src/config/db");
 dotenv.config();
+const AuthRouter  = require('./src/Routes/AuthRouter');
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+
+
+app.get('/ping', (req,res)=>{
+  res.send('welcome');
+}) 
 
 app.use("/healthcheck", (req, res) => {
   res.status(200).json({ message: "Everything is working as expected" });
