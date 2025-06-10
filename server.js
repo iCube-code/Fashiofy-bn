e81ad192b6121bad213a5e7c0fe78bd42e8cac38
kiran <sirlapu123@gmail.com>
2025-06-10 12:53:18 +0530

    registration api is created

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const InitializeDB = require("./src/config/db");
 dotenv.config();
+
+const registerRouter_v1 = require('./src/Routes/signup');
 const loginRouter_v1 = require('./src/Routes/login');
+const globalErrorHandler = require("./src/middlewares/globalErrorHandler");
 
 const app = express();
 const PORT = process.env.PORT ?? 8080;
@@ -19,6 +22,21 @@ app.use('/api', loginRouter_v1);
 app.use("/healthcheck", (req, res) => {
   res.status(200).json({ message: "Everything is working as expected" });
 });
+
+// //Root Endpoint
+// app.get("/", (req,res) => {
+//   res.json({message: "Hello from Fashiofy server!"});
+// })
+
+// Middlewares
+app.use(express.json()); // parse the incomming req into JSON formate
+
+// other Endpoints
+app.use("/api/user", registerRouter_v1);
+
+// Global error handler
+app.use(globalErrorHandler);
+
 
 app.listen(PORT, () => {
   try {
--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -1,44 +1,88 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
-      required: true,
+      required: [true, "first name is required"],
       trim: true,
+      minlength: [3, "firstname minmam 3 charecters"],
+      maxlength: [50, "firstname maximum 50 charecters"],
     },
+
     lastName: {
       type: String,
-      required: true,
+      required: [true, "Last name is required"],
       trim: true,
+      minlength: [2, "Last name must be at least 2 characters"],
+      maxlength: [50, "Last name cannot exceed 50 characters"],
     },
+
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
       trim: true,
       lowercase: true,
-      unique:true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+        },
+        message: "Please enter a valid email address",
+      },
+      
     },
-    phoneNumber: {
-      type: Number,
-      required: true,
-    },
+
     password: {
       type: String,
-      required: true,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters"],
+
+      match: [
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,}$/,
+        "Password must contain uppercase, lowercase, number, and special character",
+      ],
     },
-    isActive: {
+
+    phoneNumber: {
       type: String,
+      required: [true, "Phone number is required"],
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^\d{10}$/.test(v); // exactly 10 digits
+        },
+        message: "Phone number must be exactly 10 digits",
+      },
     },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
     fk_role_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Role",
+      // required: [true, "Role is required"],
     },
   },
+
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+
+});
 
 const User = mongoose.model("User", userSchema);
 
