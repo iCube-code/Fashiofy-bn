2d6af6d27c565c91154b3975446d6b8121044f6a
Manoj-H1180 <hostinghero5@gmail.com>
2025-06-13 05:46:26 +0530

    Implemented Logging feature

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const InitializeDB = require("./src/config/db");
+const logger = require("./src/utils/logger");
+const { InitializeLog } = require("./src/middleware/logMiddleware");
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(InitializeLog);
 
 app.use("/healthcheck", (req, res) => {
   res.status(200).json({ message: "Everything is working as expected" });
@@ -12,10 +19,10 @@ app.use("/healthcheck", (req, res) => {
 
 app.listen(PORT, () => {
   try {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    logger.info(`Server is running on http://localhost:${PORT}`);
     InitializeDB();
   } catch (error) {
-    console.log(error);
+    logger.error(error);
     process.exit(1);
   }
 });
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const logger = require("../utils/logger");
 
 async function InitializeDB() {
   try {
     await mongoose.connect(process.env.DB_URL);
-    console.log("Connected to Database");
+    logger.info("Connected to Database");
   } catch (error) {
-    console.error(error);
+    logger.error(error);
     process.exit(1);
   }
 }
