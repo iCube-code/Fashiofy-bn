129af8ebb82b0156c0af2e0f7be96ed867fea742
Rajitha <rajitha.jaltary@gmail.com>
2025-07-02 22:50:50 +0530

    changes in auth

--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -3,24 +3,25 @@ const logger = require("../utils/logger.js");
 const User = require('../model/UserModel.js');
 
 const authMiddleWare = async (req, res, next) => {
+
     try {
         const token = req.headers.authorization.split(' ')[1];
-       
         if (!token) {
             logger.error("No token provided");
             return res.status(401).json({ message: "No token provided" });
         }
-        const decodedToken = jwt.verify(token,process.env.JWT_SECRET);        
-        const user = await User.findById(decodedToken.id);
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decodedToken.userId);
         req.user = user;
         next();
 
-    } catch (error) {
+    }
+    catch (error) {
         if (error.name === 'TokenExpiredError') {
             logger.error("Token expired", error);
             return res.status(401).json({ message: "Token expired", expiredAt: error.expiredAt });
         }
-        logger.error("Invalid Token",error);
+        logger.error("Invalid Token", error);
         return res.status(401).json({ message: "Invalid Token" })
     }
 }
